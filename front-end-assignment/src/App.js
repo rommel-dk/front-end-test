99279a1eb39a1897b1e9dba911253a2d7c1dff9e
Usman Kiyani <uskiyani@gmail.com>
2020-09-11 12:31:39 +0200

    base typography setup

--- front-end-assignment/src/App.js
+++ front-end-assignment/src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      Hallo
+      
+      
+      <h1>Master the front-end with finesse</h1>
+
+<h2>The assignment is supposed to be executed as simple as possible. Code the template in HTML/CSS and try to avoid 3. party frameworks as much as possible. Creating the page with React components will grant extra megamate bonus points.</h2>
+
+      <p>The assignment must be delivered as a pull request to the assignment
+repository and we appreciate running commits so we can see how you
+go from our small layout to a final awesome product.</p>
     </div>
   );
 }
