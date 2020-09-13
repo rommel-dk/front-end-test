e7c3e44c236a119a1d5308ab7a8040a8f4e546cd
Tetiana Zaslon <zaslontania@gmail.com>
2020-09-13 12:24:16 +0200

    Adding Showcase block. Mobile style

---
+++ components/Showcase.js
@@ -0,0 +1,15 @@
+function Showcase(props) {
+  return (
+    <div className="showcase-wrapper">
+      <div className="showcase-image">
+        <div className="showcase-image_inner"></div>
+      </div>
+      <p className="showcase-description">
+        The assignment is supposed to be executed as simple as possible. Code
+        the template in HTML/CSS and try to avoid 3. party frameworks as much as
+        possible. Creating the page with React components will grant extra
+        megamate bonus points.
+      </p>
+    </div>
+  );
+}
