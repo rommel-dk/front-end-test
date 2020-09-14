68cc4b17716173cfc37d4d62246112e5a985ec20
Usman Kiyani <uskiyani@gmail.com>
2020-09-14 11:48:26 +0200

    hero compoent done + Arrow btn

---
+++ front-end-assignment/src/components/arrow/arrowRD.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './arrow.css';
+
+const ArrowRD = () => {
+  return (
+    <div className="arrow-rd-container">
+      <div className="arrow-rd-wrapper u-h-wrapper">
+        <div className="arrow-rd">
+          <span className="arrow-rd-inner">
+            <span className="arrow-rd__arrow"></span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ArrowRD
