fde8f18015b070ce0809904b39ff0c61d42fe2c3
JULIA TREFAS <trefasjulia@gmail.com>
2020-09-16 18:09:38 +0200

    style hero component details

--- master-the-front-end/src/components/Hero/index.js
+++ master-the-front-end/src/components/Hero/index.js
@@ -2,14 +2,41 @@ import React from "react";
 import styles from "./hero.module.scss";
 
 function Hero(props) {
+  const titleClone = props.hero.title
+    .replace("end", "<br>")
+    .replace("i", "<span style='visibility:hidden;'>i</span>")
+    .replace("sse", "<br>");
   return (
     <article className={`article ${styles.article}`}>
-      <svg className="lines-group" height="75vh" width="100%">
-        <line className="line line-1" x1="100%" y1="40" x2="0" y2="500" />
-        <line className="line line-1" x1="100%" y1="300" x2="0" y2="500" />
-        <line className="line line-2" x1="100%" y1="600" x2="0" y2="350" />
+      <svg className={styles.lines_group} height="80%" width="100vw">
+        <line
+          className="line line-1"
+          x1="101%"
+          y1="4%"
+          x2="-1%"
+          y2="78%"
+        ></line>
+        <line
+          className="line line-1"
+          x1="101%"
+          y1="45%"
+          x2="-1%"
+          y2="80%"
+        ></line>
       </svg>
-      <h1>{props.hero.title}</h1>
+      <svg className={styles.line_single} height="80%" width="100vw">
+        <line
+          className="line line-2"
+          x1="101%"
+          y1="92%"
+          x2="-1%"
+          y2="53%"
+        ></line>
+      </svg>
+      <div className={styles.title_group}>
+        <h1>{props.hero.title}</h1>
+        <h1 dangerouslySetInnerHTML={{ __html: titleClone }}></h1>
+      </div>
       <p>{props.hero.description}</p>
       <a className="link" href="#tiles">
         <svg width="110">
