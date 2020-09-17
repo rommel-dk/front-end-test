881fb3a5b14845537f86e0ebb29ab7b5940dd74f
benada002 <45796304+benada002@users.noreply.github.com>
2020-09-18 01:48:16 +0200

    Add basic styling to Hero1 and add it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Hero1 from './components/Hero1';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Hero1 title="Master the front-end with finesse">
+      The assignment is supposed to be executed as simple as possible. Code the template in HTML/CSS and try to avoid 3. party frameworks as much as possible. Creating the page with React components will grant extra megamate bonus points.
+      </Hero1>
     </div>
   );
 }
