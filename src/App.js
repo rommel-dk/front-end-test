161820dceda0a3b4e4b28bcbc3c6dae741cf3c4c
amjadiftikhar <amjadiftikhar@github.com>
2020-09-17 16:11:27 +0200

    extra stuff been removed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./global.styles.css"
+import Section from "./components/section.components/Section";
+import Header from "./components/header.components/Header";
+import SubHeader from "./components/header.components/SubHeader";
+import assignmentData from "./assignment-assets/data.json";
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <Header 
+          title={assignmentData['hero-1'].title} 
+          description={assignmentData['hero-1'].description}
+        />
+      </div>
+        <Section/>
+      <div className="sub-section">
+        <SubHeader 
+          title={assignmentData['hero-2'].title} 
+          subtitle={assignmentData['hero-2'].subtitle}
+          description={assignmentData['hero-2'].description}
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
