5b014119377462a2d70c6fa7fa685de5445e575e
benada002 <45796304+benada002@users.noreply.github.com>
2020-09-18 14:43:53 +0200

    Make GetNotOverlapped it's own component

--- src/components/Hero1/index.tsx
+++ src/components/Hero1/index.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 import {ReactComponent as Background1} from '../../svgs/hero1Background1.svg';
 import {ReactComponent as Background2} from '../../svgs/hero1Background2.svg';
+import GetNotOverlapped from '../GetNotOverlapped';
 
 import styles from './Hero1.module.scss';
-
-const GetNotOverlapped = ({str = ''}) => {
-    if(typeof str !== 'string'|| str === '')
-        return null;
-    
-    return <span className={styles['not-overlapped']}>{str}</span>;
-}
 
 interface IHero1{
     title: string;
@@ -20,11 +14,11 @@ interface IHero1{
 export default function Hero1({title, children}: IHero1){
     const usableTitle = <>
         {title[0]}
-        <GetNotOverlapped str={title.slice(1, 16)} />
+        <GetNotOverlapped str={title.slice(1, 16)} className={styles['not-overlapped']} />
         {title.slice(16, 21)}
-        <GetNotOverlapped str={title[21]} />
+        <GetNotOverlapped str={title[21]} className={styles['not-overlapped']} />
         {title[22]}
-        <GetNotOverlapped str={title.slice(23, 30)} />
+        <GetNotOverlapped str={title.slice(23, 30)} className={styles['not-overlapped']} />
         {title.slice(30)}
     </>;
 
