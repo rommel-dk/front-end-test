b12d176f9a38e47bb2f644de8cbf62a5c89a9c94
benada002 <45796304+benada002@users.noreply.github.com>
2020-09-19 14:51:16 +0200

    Change index.css to scss

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
