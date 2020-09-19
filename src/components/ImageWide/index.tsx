f42348ca05383fe8fd987f5277b72111bcd68c48
benada002 <45796304+benada002@users.noreply.github.com>
2020-09-19 16:12:59 +0200

    [ImageWide] Add basic structure and styling

--- src/components/ImageWide/index.tsx
+++ src/components/ImageWide/index.tsx
@@ -1,1 +1,11 @@
-import React from 'react';
+import React, { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
+
+import styles from './ImageWide.module.scss'
+
+export default function ImageWide(props: DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>){
+    return (
+        <section className={styles['image-wide']}>
+            <img className={styles.image} {...props} />
+        </section>
+    );
+}
