d54f42d6c8605c00ff152e5c6cd5ff2d4d3efb62
benada002 <45796304+benada002@users.noreply.github.com>
2020-09-20 03:56:41 +0200

    [ImageText] Add basic structure and styling

--- src/components/ImageText/index.tsx
+++ src/components/ImageText/index.tsx
@@ -1,1 +1,24 @@
 import React from 'react';
+
+import {ReactComponent as Bar} from '../../svgs/imageTextBar.svg';
+import {ReactComponent as Arrow} from '../../svgs/footerArrow.svg';
+
+
+import styles from './ImageText.module.scss';
+
+export default function ImageText(){
+    return (
+        <section className={styles['image-text']}>
+            <img className={styles.image} src="https://raw.githubusercontent.com/rommel-dk/front-end-test/master/assignment-assets/images/text-media-right.png" alt="" />
+            <div className={styles.content}>
+                <div className={styles['text-container']}>
+                    <p className={styles.text}>
+                        We use tools like NPM/Gulp/Webpack to preprocess files and PostCSS for some mean, lean, killing machine code and React JS is taking over WordPress one step at a time.
+                    </p>
+                    <Bar className={styles.bar} />
+                </div>
+                <Arrow className={styles.arrow} />
+            </div>
+        </section>
+    );
+}
