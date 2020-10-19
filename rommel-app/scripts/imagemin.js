02961066603dbe059a8c63b57c2a738e2df37c72
Nurgiel <artur.fidelus@gmail.com>
2020-10-19 17:56:22 +0200

    Static assets, imagemin-webp script

---
+++ rommel-app/scripts/imagemin.js
@@ -0,0 +1,24 @@
+const imagemin = require('imagemin')
+const imageminWebp = require('imagemin-webp');
+
+/*
+imagemin(input, options?)
+Returns Promise<object[]> in the format {data: Buffer, sourcePath: string, destinationPath: string}.
+*/
+
+(async () => {
+  await imagemin(['./public/images/*.{jpg,png}'], {
+    destination: './public/images',
+    plugins: [
+      imageminWebp({ quality: 50 })
+    ]
+  }).then(files => {
+    if (Array.isArray(files) && files.length) {
+      console.log('Images optimised.')
+    } else {
+      console.log('No files found for conversion.')
+    }
+  }).catch(err => {
+    console.log(err)
+  });
+})()
